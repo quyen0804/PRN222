e6c644e667b870f13922cbe642b5937ea5982ea5
ThucQuyen <quyenntt0804@gmail.com>
2025-01-17 13:27:39 +0700

    lab1,2

---
+++ Lab1/ServerApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerApp
+{
+    internal class Program
+    {
+        static void ProcessMessage(Object parm) {
+            string data;
+            int count;
+            try
+            {
+                TcpClient client = parm as TcpClient;
+                // Buffer for reading data
+                Byte[] bytes = new byte[256];
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+                // Loop to receive all the data sent by the client.
+                while((count = stream.Read(bytes, 0, bytes.Length)) != 0){
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, count);
+                    Console.WriteLine($"Received: {data} at {DateTime.Now:t}");
+                    // Process the data sent by the client.
+                    data = $"{data.ToUpper()}";
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                    // Send back a respone.
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine($"Sent: {data}");
+                }
+                // Shutdown and end connection
+                client.Close();
+            }
+            catch (Exception ex){
+                Console.WriteLine("{0}", ex.Message);
+                Console.WriteLine("Waiting message...");
+            }
+        }
+        static void ExecuteServer (string host, int port) {
+            int Count = 0;
+            TcpListener server = null;
+            try
+            {
+                Console.Title = "Server Application";
+                IPAddress localAddr = IPAddress.Parse(host);
+                server = new TcpListener(localAddr, port);
+                // Start listening for client requests.
+                server.Start();
+                Console.WriteLine(new string('*', 40));
+                Console.WriteLine("Waiting for a connection... "); ;
+                while (true)
+                {
+                    // Perform a blocking call to accept requests.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine($"Number of client connected: {++Count}");
+                    Console.WriteLine(new String('*', 40));
+                    // Create a thread to receive and send message
+                    Thread thread = new Thread(new ParameterizedThreadStart(ProcessMessage));
+                    thread.Start(client);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0}", ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+                Console.WriteLine("Server stopped. Press any key to exit !");
+            }
+            Console.Read();
+        }
+        static void Main(string[] args)
+        {
+            string host = "127.0.0.1";
+            // Set the TcpListener on port 13000.
+            int port = 13000;
+            ExecuteServer(host, port);
+        } // End Main
+    }
+}
---
+++ ThreadDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void PrintNumber(string message)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"{message}:{i}");
+            Thread.Sleep(1000);
+        }
+    }
+
+    static void Main(string[] args) {
+        Thread.CurrentThread.Name = "Main";
+        //create a task by using a lambda expression
+        Task task01 = new Task(() => PrintNumber("Task 1"));
+        task01.Start();
+        //create a task by using delegate and run the task
+        Task task02 = Task.Run(delegate
+        {
+            PrintNumber("Task 02");
+        });
+        //create a task by using a action delegate
+        Task task03 = new Task(new Action(() =>
+        {
+            PrintNumber("Task 03");
+        }));
+        task03.Start();
+        Console.WriteLine($"Thread '{Thread.CurrentThread.Name}'");
+        Console.ReadKey();
+    }
+}
